3517de999054630bbb4381c071d636de9b3f0e40
emanueletoffolon <emanuele.toffolon@enesi.it>
2023-06-13 10:45:59 +0200

    Update Api.php

--- src/Api.php
+++ src/Api.php
@@ -5,63 +5,70 @@ namespace Enesisrl\Qapla;
 use Enesisrl\Qapla\RestClient;
 
 class Api extends RestClient {
-    protected $api_version;
-
-    protected $api_key;
-
-    protected $sandbox;
 
     public function __construct(array $options = []){
-        parent::__construct($options);
 
-        if (isset($options['api_version'])) {
-            $this->setApiVersion($options['api_version']);
-        }
-        if (isset($options['api_key'])) {
-            $this->setApiKey($options['api_key']);
-        }
-        if (isset($options['sandbox'])) {
-            $this->setSandbox($options['sandbox']);
-        }
+        $default_options = [
+            'headers' => [],
+            'parameters' => [],
+            'curl_options' => [],
+            'build_indexed_queries' => FALSE,
+            'api_version' => '1.2',
+            'api_key' => null,
+            'sandbox' => null,
+            'user_agent' => "Enesi QaplaClient/1.0",
+            'base_url' => "https://api.qapla.it/",
+            'format' => NULL,
+            'format_regex' => "/(\w+)\/(\w+)(;[.+])?/",
+            'decoders' => [
+                'json' => 'json_decode',
+                'php' => 'unserialize'
+            ],
+            'username' => NULL,
+            'password' => NULL
+        ];
+
+        $final_settings = array_merge($default_options, $options);
+        $final_settings['base_url'] = implode('/',[rtrim($final_settings['base_url'],"/"),$final_settings['api_version']]);
+        parent::__construct($final_settings);
     }
 
     /**
      * @return mixed
      */
     public function getApiKey(){
-        return $this->api_key;
-    }
-    /**
-     * @param mixed $api_key
-     */
-    public function setApiKey($api_key){
-        $this->api_key = $api_key;
+        return $this->options['api_key'];
     }
 
     /**
      * @return mixed
      */
     public function getApiVersion(){
-        return $this->api_version;
-    }
-
-    /**
-     * @param mixed $api_version
-     */
-    public function setApiVersion($api_version){
-        $this->api_version = $api_version;
+        return $this->options['api_version'];
     }
 
     /**
      * @return mixed
      */
     public function getSandbox(){
-        return $this->sandbox;
+        return $this->options['sandbox'];
     }
-    /**
-     * @param mixed $sandbox
-     */
-    public function setSandbox($sandbox){
-        $this->sandbox = $sandbox;
+
+    public function pushOrder($params=[]){
+
+        $default_data = [
+            'apiKey' => $this->getApiKey(),
+            'origin' => 'ecommerce',
+            'pushOrder' => [
+
+            ]
+        ];
+
+        $data = array_merge($default_data,$params);
+
+        return $this->post('pushOrder', $data);
+
     }
+
+
 }
